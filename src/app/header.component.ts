1480af5a566b196367aa5454e4926f7e57f3a5a4
radhemuchi <prnkpawar773@gmail.com>
2017-10-15 20:00:48 +0530

    marging-top

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     templateUrl:'./header.component.html',
     selector:'app-header',
-    styles:["li{cursor:pointer; margin-top:5%; margin-right:10px} "]
+    styles:["li{cursor:pointer; margin-top:10%; margin-right:10px} "]
 })
 export class HeaderComponent{
 

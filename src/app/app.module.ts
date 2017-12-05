0cc860e23bbb36a3ae9fcc7f6b6da2120f0e7fd9
radhemuchi <prnkpawar773@gmail.com>
2017-12-05 21:09:53 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import{RouterModule}from '@angular/router';
 import {FormsModule} from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import{HeaderComponent} from './header.component';
 import{FooterComponent} from './footer.component';
